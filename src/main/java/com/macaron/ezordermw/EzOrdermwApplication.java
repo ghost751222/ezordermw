ebf25f8d7e75d37e1a58fcd667cdcf25c43948b4
ghost751222@gmail.com <ghost751222@gmail.com>
2024-12-29 23:30:49 +0800

    test modify

--- src/main/java/com/macaron/ezordermw/EzOrdermwApplication.java
+++ src/main/java/com/macaron/ezordermw/EzOrdermwApplication.java
@@ -9,9 +9,9 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource({"classpath:persistence.properties"})
 public class EzOrdermwApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-
-		SpringApplication.run(EzOrdermwApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("test");
+        SpringApplication.run(EzOrdermwApplication.class, args);
+    }
 
 }
